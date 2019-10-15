f42c8d0444962d12fec3346648b7ebd10e5ac37f
vanebustillos <vanebustillos1@gmail.com>
2019-10-14 22:45:32 -0400

    Terminando Registro Jugadores

---
+++ Juego4enRaya/Juego4enRaya/Registro.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace Juego4enRaya
+{
+    partial class Registro
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Registro));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.PrimerJugador = new System.Windows.Forms.TextBox();
+            this.SegundoJugador = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft PhagsPa", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(8, 125);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(156, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Primer Jugador:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft PhagsPa", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(69, 209);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(177, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Segundo Jugador:";
+            // 
+            // PrimerJugador
+            // 
+            this.PrimerJugador.Location = new System.Drawing.Point(170, 130);
+            this.PrimerJugador.Name = "PrimerJugador";
+            this.PrimerJugador.Size = new System.Drawing.Size(140, 20);
+            this.PrimerJugador.TabIndex = 2;
+            // 
+            // SegundoJugador
+            // 
+            this.SegundoJugador.Location = new System.Drawing.Point(252, 214);
+            this.SegundoJugador.Name = "SegundoJugador";
+            this.SegundoJugador.Size = new System.Drawing.Size(130, 20);
+            this.SegundoJugador.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(145, 262);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 40);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "JUGAR";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Registro
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(399, 335);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.SegundoJugador);
+            this.Controls.Add(this.PrimerJugador);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Registro";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox PrimerJugador;
+        private System.Windows.Forms.TextBox SegundoJugador;
+        private System.Windows.Forms.Button button1;
+    }
+}
